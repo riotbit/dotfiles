f62c88d1b7abe81f3fd39dc8ca3a8ae9b25069f6
Christian Klopp <christian.klopp@fokus.fraunhofer.de>
2017-09-16 03:27:36 +0200

    fix

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -9,6 +9,14 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install fzf python-pip getantibody/tap/antibody zsh ripgrep 
 fi
 
+# Install powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
+cur_dir=$(pwd)
+cd /tmp/fonts
+./install.sh
+cd ${cur_dir}
+rm -Rf fonts
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --bin
 
@@ -17,6 +25,9 @@ curl -sL https://git.io/antibody | bash -s
 ln -s $DOTDIR/zsh/zshrc ~/.zshrc
 
 chsh -s $(which zsh) $USER
+zsh
+
+
 
 
 
