1417b7407061151796ad9809c026a6411043ce45
Christian Klopp <christian.klopp@fokus.fraunhofer.de>
2017-09-16 02:41:35 +0200

    fix

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,9 +4,17 @@ export DOTDIR="~/.dotdir"
 
 # Requirements
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo apt install fzf python-pip antibody zsh ripgrep 
+    sudo apt install python-pip zsh cargo
+    cargo install ripgrep
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install fzf python-pip antibody zsh ripgrep 
+    brew install fzf python-pip getantibody/tap/antibody zsh ripgrep 
 fi
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --bin
+
+curl -sL https://git.io/antibody | bash -s
+
+
 
 
