c1314c18e8e881b85a1f86c8ee121f1e90684bf4
Christian Klopp <christi4nk@googlemail.com>
2018-06-26 13:15:45 +0200

    various

--- update.sh
+++ update.sh
@@ -8,4 +8,10 @@ nvim +PlugUpgrade +qall
 echo "Updating npm..."
 npm -g upgrade
 echo "Start Tmux and press: tmux Ctrl-a U"
-
+echo "Update relevant python packages"
+pyenv deactivate
+pyenv local 2.7.12
+sudo -H pip2 install -U pip jedi flake8 importmagic autopep8 yapf epc json-rpc service_factory
+pyenv local 3.6.4
+sudo -H pip3 install -U pip jedi flake8 importmagic autopep8 yapf epc json-rpc service_factory
+pyenv local --unset
