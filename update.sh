93bf2609047675c7a3246f648d319e73fdd1620c
Christian Klopp <christian.klopp@fokus.fraunhofer.de>
2019-03-01 20:48:39 +0100

    new zsh

--- update.sh
+++ update.sh
@@ -1,20 +1,49 @@
 #!/bin/zsh
 
-echo "Update emacs..."
+echo "
+================================
+Emacs
+================================
+"
 cd ${HOME}/.emacs.d && git pull --rebase
-echo "Update emacs packages..."
-echo "Updating neovim plugins..."
+
+echo "
+================================
+Neovim
+================================
+"
 nvim +PlugUpdate +qall
 nvim +PlugUpgrade +qall
-echo "Updating npm..."
-npm -g upgrade
-echo "Start Tmux and press: tmux Ctrl-a U"
 
-echo "Update relevant python packages"
+echo "
+================================
+npm
+================================
+"
+npm -g upgrade
+
+echo "
+================================
+Tmux
+================================
+"
+${HOME}/.tmux/plugins/tpm/bin/update_plugins all
+
+echo "
+================================
+Python
+================================
+"
 pyenv deactivate
 pyenv local 2.7.15 && sudo -H pip install -U pip && sudo -H pip install -U jedi flake8 importmagic autopep8 yapf epc json-rpc service_factory isort pycodestyle
 pyenv local 3.7.1 && sudo -H pip install -U pip && sudo -H pip install -U jedi flake8 importmagic autopep8 yapf epc json-rpc service_factory isort pycodestyle
 pyenv local --unset
 
-source ${ZPLUG_HOME}/init.zsh
-zplug update
+echo "
+================================
+Zsh
+================================
+"
+source '/home/ckl/.zplugin/bin/zplugin.zsh'
+zplugin self-update
+zplugin update
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -5,30 +5,16 @@ DOTDIR=${HOME}/.dotfiles
 # Requirements
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if command -v apt >/dev/null; then
-        sudo apt install python-pip zsh curl zplug
+        echo "Requirements: sudo apt install zsh curl fonts-powerline"
     fi
 
     if command -v pacman >/dev/null; then
-        yaourt -Sy python-pip zsh zplug curl fzf python2 python2-pip pyenv pyenv-virtualenv pyenv-virtualenvwrapper
+        echo "Requirements: sudo pacman -Sy zsh curl ripgrep fonts-powerline"
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install fzf python-pip zplug zsh ripgrep
+    echo "Requirements: brew install zsh curl ripgrep"
 fi
 
-# Install powerline fonts
-git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
-cur_dir=$(pwd)
-cd /tmp/fonts
-./install.sh
-cd ${cur_dir}
-rm -Rf fonts
-
-if command -v fzf >/dev/null; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install --bin
-fi
-
-rm ${HOME}/.zshrc
 ln -s ${DOTDIR}/zsh/zshrc ${HOME}/.zshrc
 
 chsh -s $(which zsh) $USER
